4191923b1a67d05bf6012d25499ad4e4a0bc6383
mrzepisko <rz.marcin@gmail.com>
2018-03-31 23:15:52 +0200

    debug window

--- Assets/Main/Scipts/Rocket/RocketCollision.cs
+++ Assets/Main/Scipts/Rocket/RocketCollision.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace RocketLander {
     public class RocketCollision : MonoBehaviour {
-        [SerializeField] float maxSpeed = .5f, landedSpeedTreshold = 1f, touchdownTime = 3f;
+        [SerializeField] float maxSpeed = .5f, landedSpeedTreshold = 1f, touchdownMinTime = 5f;
         [SerializeField] LayerMask deathLayers, platformLayers;
+        
+#if DEBUG
+        public static float relativeForce;
+#endif
 
         //cache
         RocketEngine engine;
+        Rigidbody2D rb;
 
+#if DEBUG //very very very ugly but working for debug
+        public static
+#endif
         //counter
         float touchdown;
 
         void Awake() {
             engine = GetComponent<RocketEngine>();
+            rb = GetComponent<Rigidbody2D>();
+
             GameEvents.OnStart += EnableOnStart;
         }
 
@@ -39,26 +49,34 @@ namespace RocketLander {
                 });
                 enabled = false;
             }
+
+#if DEBUG
+            relativeForce = collision.relativeVelocity.magnitude;
+#endif
         }
 
         void OnCollisionStay2D(Collision2D collision) {
             if (!enabled) return; //high speed crash workaround
             //staying on platform layer below speed treshold
-            if (((1 << collision.collider.gameObject.layer) & platformLayers.value) > 0 
+            if (((1 << collision.collider.gameObject.layer) & platformLayers.value) > 0
                 && collision.relativeVelocity.sqrMagnitude <= landedSpeedTreshold * landedSpeedTreshold
                 && Mathf.Abs(collision.otherRigidbody.angularVelocity) < 1) {
                 touchdown += Time.fixedDeltaTime;
-                if (touchdown >= touchdownTime) {
+                if (touchdown >= touchdownMinTime) {
                     GameEvents.RocketLanded(new GameEvents.RocketTouchdown() {
                         fuelLeft = engine.FuelLeft,
                     });
                     enabled = false;
                 }
             } else {
-                touchdownTime = 0;
-            }    
+                touchdown = 0;
+            }
         }
 
+        void OnCollisionExit2D(Collision2D collision) {
+            touchdown = 0;
+        }
+        
         void EnableOnStart() {
             enabled = true;
         }
---
+++ Assets/Main/Scipts/UI/DebugUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace RocketLander {
+    [DefaultExecutionOrder(200000)]
+    public class DebugUI : MonoBehaviour {
+#if DEBUG
+        Rigidbody2D rocket;
+
+        [SerializeField] Text collisionForce;
+
+        private void Start() {
+            rocket = FindObjectOfType<RocketEngine>().GetComponent<Rigidbody2D>();
+        }
+
+        private void OnGUI() {
+            Rect rect = new Rect(0, Screen.height - 20, Screen.width, 20);
+            GUI.color = Color.red;
+            GUI.Label(rect, string.Format("Last collision: {0:0.0000}", RocketCollision.relativeForce.ToString()));
+            rect.y -= 20f; 
+            GUI.Label(rect, string.Format("Angular speed: {0:0.0000}", rocket.angularVelocity));
+            rect.y -= 20f;
+            GUI.Label(rect, string.Format("Speed: {0:0.0000}", rocket.velocity.magnitude));
+            rect.y -= 20f;
+            GUI.Label(rect, string.Format("Touchdown: {0:0.0000}s", RocketCollision.touchdown));
+        }
+#else
+        private void Awake() {
+            Destroy(gameObject);
+        }
+#endif
+    }
+}
